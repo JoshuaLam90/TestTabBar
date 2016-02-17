a8b096e5d9dd0dbd3291df71a6b2d71def521309
成(平安科技移动开发三队健康及养老险开发组) 林志 <linzhicheng190@c02q6pm4fvh5.local>
2016-02-17 11:36:56 +0800

    Initial Commit

---
+++ JKYTestTabBar/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JKYTestTabBar
+//
+//  Created by 林志成(平安科技移动开发三队健康及养老险开发组) on 16/2/17.
+//  Copyright © 2016年 PingAn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
